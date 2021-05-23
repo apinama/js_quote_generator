b3c7aad945d3d17940beea02a8b15799f7b049da
apinama <apinama985@gmail.com>
2021-05-23 21:37:17 +0300

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const spinner = document.querySelector('#js-spinner');
+
+const newQuoteButton = document.querySelector('#js-new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+    // remove the "hidden" class on the spinner
+    spinner.classList.remove('hidden');
+    // disable the quote button
+    newQuoteButton.disabled = true;
+
+    try {
+        const response = await fetch(endpoint)
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        displayQuote(json.message);
+    } catch {
+        alert('Failed to fetch new quote');
+    } finally {
+        // enable the quote button
+        newQuoteButton.disabled = false;
+        // add the "hidden" class back again
+        spinner.classList.add('hidden');
+    }
+}
+
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
+}
